109d95b9803462f3e01151d5837d6eddab135044
sumanthdynpro <sumanth.m@dynpro.in>
2025-11-14 14:32:47 +0530

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -20,7 +20,7 @@ export default defineConfig({
 
     // Allow external domains to access the Vite dev server
     allowedHosts: [
-      'nextbi.dynprocloud.com:3000',
+      'nextbi.duckdns.org',
       '35.174.81.186'
     ],
 
